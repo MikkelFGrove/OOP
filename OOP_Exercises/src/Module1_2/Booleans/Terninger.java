0579667670dd8bc1312924423686739973d9e9df
Mikkel Grove <mikkelfgrove@gmail.com>
2023-09-11 11:17:56 +0200

    New Files

--- OOP_Exercises/src/Module1_2/Booleans/Terninger.java
+++ OOP_Exercises/src/Module1_2/Booleans/Terninger.java
@@ -1,4 +1,9 @@
 package Module1_2.Booleans;
 
 public class Terninger {
+    public static void main(String[] args) {
+        int dice = 5;
+        boolean evenNBig = dice > 3 && dice % 2 == 0;
+        System.out.println(evenNBig);
+    }
 }
