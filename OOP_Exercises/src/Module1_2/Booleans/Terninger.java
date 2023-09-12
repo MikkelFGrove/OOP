155d1ebe69b08b413be447a224488e6266e520e1
Mikkel Grove <mikkelfgrove@gmail.com>
2023-09-12 17:33:04 +0200

    New comments

--- OOP_Exercises/src/Module1_2/Booleans/Terninger.java
+++ OOP_Exercises/src/Module1_2/Booleans/Terninger.java
@@ -2,8 +2,14 @@ package Module1_2.Booleans;
 
 public class Terninger {
     public static void main(String[] args) {
+
+        // Initializing variable
         int dice = 5;
+
+        // Checks if dice is bigger than 3 and if it is even
         boolean evenNBig = dice > 3 && dice % 2 == 0;
+
+        // Prints out the result of evenNBig
         System.out.println(evenNBig);
     }
 }
