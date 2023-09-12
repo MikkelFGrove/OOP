0e538158f55cf7821fe074377e07657a8cca99b8
Mikkel Grove <mikkelfgrove@gmail.com>
2023-09-12 10:18:43 +0200

    PrimeNumber added

--- OOP_Exercises/src/Module1_2/Loops/PrimeNumber.java
+++ OOP_Exercises/src/Module1_2/Loops/PrimeNumber.java
@@ -2,15 +2,25 @@ package Module1_2.Loops;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        boolean is_prime;
+        int limit = 1000000;
+        int largestPrime = 0;
 
-        for (int i=3 ; i<100 ; i++){
-            for (int j=1 ; j<i ; j++){
-                if(i%j == 0){
-                    is_prime = false;
-                }else break;
-                System.out.println(is_prime);
+        for (int num = 3; num <= limit; num++) {
+            boolean isPrime = true;
+
+            for (int divisor = 2; divisor <= Math.sqrt(num); divisor++) {
+                if (num % divisor == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                largestPrime = num;
             }
         }
+
+        System.out.println("The largest prime number up to " + limit + " is: " + largestPrime);
+
     }
 }
