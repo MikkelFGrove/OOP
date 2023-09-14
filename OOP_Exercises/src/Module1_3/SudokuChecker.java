cc6882eb65afb7a0a96947fd350015d3f2fbc524
Mikkel Grove <mikkelfgrove@gmail.com>
2023-09-14 10:35:24 +0200

    SudokuChecker

--- OOP_Exercises/src/Module1_3/SudokuChecker.java
+++ OOP_Exercises/src/Module1_3/SudokuChecker.java
@@ -3,7 +3,7 @@ public class SudokuChecker {
 		int problem = 0;
 		int[][] sudokuPuzzle = 
 			{
-			{2, 2, 3, 6, 7, 8, 9, 4, 5}, 
+			{1, 2, 3, 6, 7, 8, 9, 4, 5}, 
 			{5, 8, 4, 2, 3, 9, 7, 6, 1}, 
 			{9, 6, 7, 1, 4, 5, 3, 2, 8}, 
 			{3, 7, 2, 4, 6, 1, 5, 8, 9}, 
@@ -24,36 +24,70 @@ public class SudokuChecker {
 				}
 			}
 		}
+			if (problem == 1) {
+					System.out.println("The board is not filled!");
+			}
 
 		// Check if rows are correct
 		for (int row = 0; row < sudokuPuzzle.length; row++) {
+			boolean[] taken = new boolean[9];
 			for (int coloumn = 0; coloumn < sudokuPuzzle[row].length; coloumn++){
-				boolean[] taken = {false, false, false, false, false, false, false, false, false, };
 				int currentNr = sudokuPuzzle[row][coloumn];
 				if (taken[currentNr-1] == true){
 					problem = 2;
-					System.out.println("Problem detected!!!");
 					break;
 				}else {
 					taken[currentNr-1] = true;
-					System.out.println("row: " + row + "coloumn: " + coloumn);
 				}
 
 			}
 		}
 
-		switch(problem){
-			case 1: 
-				System.out.println("Problem with If Filled");
-				break;
-
-			case 2:
-				System.out.println("Problem with Row Check");
-				break;
-
-			default:
-				System.out.println("This is a valid board");
+		if(problem == 2){
+			System.out.println("Rows are wrong!");
 		}
+
+		for (int coloumn = 0; coloumn < sudokuPuzzle.length; coloumn++) {
+			boolean [] taken = new boolean[9];
+			for (int row = 0; row < sudokuPuzzle.length; row++) {
+				int currentNr = sudokuPuzzle[row][coloumn];
+				if (taken[currentNr-1] == true){
+					problem = 3;
+					break;
+				}else {
+					taken[currentNr-1] = true;
+				}
+			}
+		}
+
+		if(problem == 3){
+			System.out.println("Coloumns are wrong!");
+		}
+
+		for (int row = 0; row < sudokuPuzzle.length; row += 3){
+			for (int coloumn = 0; coloumn < sudokuPuzzle[row].length; coloumn += 3) {
+				boolean[] taken = new boolean[9];
+				for (int rowStart = 0; rowStart < 3; rowStart++) {
+					for (int coloumnStart = 0; coloumnStart < 3; coloumnStart++) {
+						int currentNr = sudokuPuzzle[row+rowStart][coloumn+coloumnStart];
+						if (taken[currentNr-1] == true) {
+							problem = 4;
+							break;
+						}else {
+							taken[currentNr-1] = true;
+						}
+					}
+				}
+			}
+		}
+
+
+		if (problem == 4) {
+			System.out.println("Squares are wrong!");
+		}
+
+		if(problem == 0)
+			System.out.println("This is a valid board");
 
 	}
 }
